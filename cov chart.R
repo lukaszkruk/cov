f295fb94cb377d552c0dcae83eb594a8a79fe1ac
lukaszkruk <lukasz.kruk@gmail.com>
2020-03-25 08:58:47 +0100

    tests

--- cov chart.R
+++ cov chart.R
@@ -1,5 +1,4 @@
 library(tidyverse)
-library(readr)
 
 Confirmed <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
 Deaths <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv")
@@ -12,9 +11,9 @@ tidy_CSSE <- function(data, value_col = "Confirmed"){
   data%>%
     rename('admin1' = 'Province/State',
            'admin0' = 'Country/Region')%>%
-    pivot_longer(cols = contains("/"), names_to = 'date', values_to = value_col)%>%
+    pivot_longer(cols = contains("/"), names_to = 'date_raw', values_to = value_col)%>%
     select(-Lat, -Long)%>%
-    mutate(date = as.Date(date, format = "%m/%d/%y"))
+    mutate(Date = as.Date(date_raw, format = "%m/%d/%y"))
 }
 
 tidy_CSSE_admin0 <- function(data, value_col = "Confirmed"){
@@ -22,10 +21,10 @@ tidy_CSSE_admin0 <- function(data, value_col = "Confirmed"){
   new_col <- paste0(enquo(value_col))[2]
 
   data %>%
-    group_by(admin0, date)%>%
+    group_by(admin0, Date)%>%
     summarise(!!new_col := sum(!!sym(sum_col)))%>%
     left_join(isoalpha3, by = c('admin0' = 'Country'))%>%
-    mutate(unique_id = paste0(iso_alpha3, date))%>%
+    mutate(unique_id = paste0(iso_alpha3, Date))%>%
     ungroup()
   }
 
@@ -39,19 +38,33 @@ recovered_tidy <- tidy_CSSE_admin0(tidy_CSSE(Recovered, value_col = 'Recovered')
 all_data_admin0 <- left_join(confirmed_tidy,deaths_tidy, by = c('unique_id' = 'unique_id'))%>%
   left_join(recovered_tidy, by = c('unique_id' = 'unique_id'))%>%
   select(-contains("."))%>%
-  select(admin0, iso_alpha3, date, Confirmed, Deaths, Recovered)
+  select(admin0, iso_alpha3, Date, Confirmed, Deaths, Recovered) %>%
+  group_by(admin0) %>%
+  mutate(Active = (Confirmed - (Recovered + Deaths)),
+         Day = row_number(dplyr::na_if(Confirmed, 0)),
+         `Daily percentage change` = ifelse(Confirmed > 5,
+                                            (Confirmed/lag(Confirmed)-1)*100, 
+                                            NA
+                                            ),
+         `Daily change of daily percentage change` = `Daily percentage change` - lag(`Daily percentage change`)
+         ) 
 
 
 
+chart_list = c("Switzerland", "Italy", "China", "France", "Poland", "United Kingdom", "Spain")
 
-Confirmed %>%
-  rename(Country = `Country/Region`) %>%
-  select(-c(`Province/State`)) %>% 
-  gather(day, Confirmed, 4:{dim(Confirmed)[2]-1}) %>%
-  mutate(Date = as.Date(day, format = "%m/%d/%y")) %>%
-  group_by(Country, Date) %>%
-  summarise(Lat = mean(Lat), Long = mean(Long), Confirmed = sum(Confirmed)) %>%
-  filter(Country %in% c("Switzerland", "Italy", "China", "France", "Poland", "United Kingdom")) %>%
-  group_by(Country) %>%
-  ggplot(aes(x=Date, y=Confirmed, colour=Country)) + geom_line() + theme_minimal() +
-  scale_y_log10()
+all_data_admin0 %>%
+  filter(admin0 %in% chart_list) %>%
+  ggplot(aes(x=Date, y=Confirmed, colour=admin0)) + geom_line() + theme_minimal() + 
+  scale_y_log10(labels = scales::comma) + ylab("Confirmed [log]") +
+  labs(title = "Confirmed cases by date", colour = "Country") 
+  
+ggsave(filename = "confirmed by date.png")
+
+all_data_admin0 %>%
+  filter(admin0 %in% chart_list) %>%
+  ggplot(aes(x=Day, y=Confirmed, colour=admin0)) + geom_line() + theme_minimal() + 
+#  scale_y_log10(labels = scales::comma) +
+  labs(title = "Confirmed cases by day since first case")
+
+ggsave(filename = "confirmed by day.png")
